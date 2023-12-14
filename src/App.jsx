c371a238aab38ce0fe0f239d62c37e53f09ee20d
Emilse Arias <emilsearias@gmail.com>
2023-12-14 18:45:41 -0300

    agregamos el componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import FormularioTareas from './components/FormularioTareas';
-import { Container } from 'react-bootstrap';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FormularioTareas from "./components/FormularioTareas";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
- 
   return (
-    <Container className='my-4'>
-      <h1 className='text-center display-3'>Lista de tareas</h1>
-      <FormularioTareas></FormularioTareas>
-    </Container>
-  )
+    <>
+      <Container className="my-4">
+        <h1 className="text-center display-3">Lista de tareas</h1>
+        <FormularioTareas></FormularioTareas>
+      </Container>
+      <Footer></Footer>
+    </>
+  );
 }
 
-export default App
+export default App;
