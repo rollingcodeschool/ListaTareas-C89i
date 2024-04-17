f7a5f32a9ade33bd4fc16beca23b850aa7e72625
Emilse Arias <emilsearias@gmail.com>
2024-04-17 20:32:55 -0300

    cargamos las tareas en el array tareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -3,24 +3,35 @@ import Listatareas from "./Listatareas";
 import { useState } from "react";
 
 const FormularioTareas = () => {
- const [nombreTarea, setNombreTarea ] = useState('');
+  const [nombreTarea, setNombreTarea] = useState("");
+ const [tareas, setTareas] = useState([])
 
+const handleSubmit = (e)=>{
+  e.preventDefault();
+// tareas.push(nombreTarea)
+setTareas([...tareas, nombreTarea]); //operador spread
+//limpiar el input
+setNombreTarea('');
+}
 
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
           <Form.Control
             type="text"
             placeholder="Ej: tarea 1"
             minLength={3}
             maxLength={50}
-         onChange= {(e)=> setNombreTarea(e.target.value) }
+            onChange={(e) => setNombreTarea(e.target.value)}
+            value={nombreTarea}
           />
-          <Button variant="primary" className="mx-2" type="submit">Enviar</Button>
+          <Button variant="primary" className="mx-2" type="submit">
+            Enviar
+          </Button>
         </Form.Group>
       </Form>
-      <Listatareas ></Listatareas>
+      <Listatareas></Listatareas>
     </section>
   );
 };
