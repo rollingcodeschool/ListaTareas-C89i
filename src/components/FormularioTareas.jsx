ae0f0ccdb9e1b303150e48d0f9200208b6b0a439
Emilse Arias <emilsearias@gmail.com>
2024-04-22 21:22:30 -0300

    agregamos la validacion de campo requerido, min y maxlength en el nombre de la tarea

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,25 +1,40 @@
 import { Button, Form } from "react-bootstrap";
 import Listatareas from "./Listatareas";
 import { useEffect, useState } from "react";
-
+import { useForm } from "react-hook-form";
 
 const FormularioTareas = () => {
-  const [nombreTarea, setNombreTarea] = useState("");
-  const [tareas, setTareas] = useState( JSON.parse(localStorage.getItem('listaTareasKey')) || [] );
+  const [tareas, setTareas] = useState(
+    JSON.parse(localStorage.getItem("listaTareasKey")) || []
+  );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
   // aqui agrego el codigo del ciclo de vida
-  useEffect(()=>{
+  useEffect(() => {
     //aqui agrear el codigo que quiero ejecutar en el montaje y act. del componente
-    console.log('estoy dentro del useEffect');
-    localStorage.setItem('listaTareasKey', JSON.stringify(tareas));
-  }, [tareas])
+    console.log("estoy dentro del useEffect");
+    localStorage.setItem("listaTareasKey", JSON.stringify(tareas));
+  }, [tareas]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (data) => {
+    console.log(data);
+    //verificar si la tarea ya existe en array de tareas
+    if(tareas.includes(data.nombreTarea)){
+      alert("La tarea ya existe")
+    }else{
+      setTareas([...tareas, data.nombreTarea]); //operador spread
+      reset();
+    }
+
     // tareas.push(nombreTarea)
-    setTareas([...tareas, nombreTarea]); //operador spread
+    // 
     //limpiar el input
-    setNombreTarea("");
+    // setNombreTarea("");
   };
 
   const borrarTarea = (nombre) => {
@@ -32,20 +47,36 @@ const FormularioTareas = () => {
 
   return (
     <section>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
           <Form.Control
             type="text"
             placeholder="Ej: tarea 1"
-            minLength={3}
-            maxLength={50}
-            onChange={(e) => setNombreTarea(e.target.value)}
-            value={nombreTarea}
+            // minLength={3}
+            // maxLength={50}
+            // onChange={(e) => setNombreTarea(e.target.value)}
+            // value={nombreTarea}
+            {...register("nombreTarea", {
+              required: "El nombre de la tarea es obligatorio.",
+              minLength: {
+                value: 3,
+                message:
+                  "El nombre de la tarea debe contener 3 caracteres como minimo.",
+              },
+              maxLength: {
+                value: 50,
+                message:
+                  "El nombre de la tarea debe contener hasta 50 caracteres como maximo.",
+              },
+            })}
           />
           <Button variant="primary" className="mx-2" type="submit">
             Enviar
           </Button>
         </Form.Group>
+        <Form.Text className="text-light">
+          {errors.nombreTarea?.message}
+        </Form.Text>
       </Form>
       <Listatareas tareasProps={tareas} borrarTarea={borrarTarea}></Listatareas>
     </section>
